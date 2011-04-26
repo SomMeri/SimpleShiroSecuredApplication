9a4980f6dc77ea883ae815edb1e20a1719fdcef5
meri <why?>
2011-04-26 11:38:51 +0200

    changed to strict antisamy policy

--- src/main/java/org/meri/simpleshirosecuredapplication/sanitizer/Sanitizer.java
+++ src/main/java/org/meri/simpleshirosecuredapplication/sanitizer/Sanitizer.java
@@ -2,6 +2,7 @@ package org.meri.simpleshirosecuredapplication.sanitizer;
 
 import java.io.InputStream;
 
+import org.owasp.appsensor.errors.AppSensorException;
 import org.owasp.validator.html.AntiSamy;
 import org.owasp.validator.html.CleanResults;
 import org.owasp.validator.html.Policy;
@@ -12,7 +13,8 @@ import org.owasp.validator.html.ScanException;
  */
 public class Sanitizer {
 
-	private static final String POLICY_FILE_LOCATION = "/antisamy-tinymce-1.4.4.xml";
+	//private static final String POLICY_FILE_LOCATION = "/antisamy-tinymce-1.4.4.xml";
+	private static final String POLICY_FILE_LOCATION = "/antisamy-slashdot-1.4.4.xml";
 	private static Policy policy;
 
 	public Sanitizer() {
@@ -22,6 +24,9 @@ public class Sanitizer {
 		AntiSamy as = new AntiSamy();
 		try {
 	    CleanResults cr = as.scan(dirtyInput, getPolicy());
+	    if (cr.getErrorMessages()==null || cr.getErrorMessages().isEmpty()) {
+	    	new AppSensorException("eventCode", "userMessage", "logMessage");
+	    }
 	    return cr.getCleanHTML();
     } catch (ScanException e) {
       throw new RuntimeException(e);
