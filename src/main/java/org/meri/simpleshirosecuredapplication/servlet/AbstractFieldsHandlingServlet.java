1a48e9d94856523bed91a707f1a73984418f7314
meri <why?>
2011-05-21 11:03:05 +0200

    XSS attack and SQL injection

--- src/main/java/org/meri/simpleshirosecuredapplication/servlet/AbstractFieldsHandlingServlet.java
+++ src/main/java/org/meri/simpleshirosecuredapplication/servlet/AbstractFieldsHandlingServlet.java
@@ -4,32 +4,40 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 
 import org.meri.simpleshirosecuredapplication.sanitizer.Sanitizer;
+import org.owasp.appsensor.AttackDetectorUtils;
 
 /**
- * Parent of all servlets reading user supplied data. Use {@link #getField(HttpServletRequest, String)}
- * to get fields values.  
+ * Parent of all servlets reading user supplied data. Use
+ * {@link #getField(HttpServletRequest, String)} to get fields values.
  * 
  */
 @SuppressWarnings("serial")
 public abstract class AbstractFieldsHandlingServlet extends HttpServlet {
 
 	private Sanitizer sanitizer = new Sanitizer();
-	
+
 	public AbstractFieldsHandlingServlet() {
 		super();
 	}
 
 	/**
-	 * Reads sanitized request parameter value from request field.
+	 * Reads sanitized request parameter value from request.
 	 * 
-	 * @param request http request
-	 * @param parameter name of request parameter 
+	 * @param request
+	 *          http request
+	 * @param parameter
+	 *          name of request parameter
 	 * 
 	 * @return request parameter value
 	 */
 	protected String getField(HttpServletRequest request, String parameter) {
-    String dirtyValue = request.getParameter(parameter);
+		String dirtyValue = request.getParameter(parameter);
+
+		// detection point: XSS, SQL injection
+		AttackDetectorUtils.verifySQLInjectionAttack(dirtyValue);
+		AttackDetectorUtils.verifyXSSAttack(dirtyValue);
+
 		return sanitizer.sanitize(dirtyValue);
-  }
+	}
 
 }
