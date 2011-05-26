da1a25c780a4443a741fa55eabe36eb8a8566c52
meri <why?>
2011-05-26 17:46:58 +0200

    formatting

--- src/main/java/org/meri/simpleshirosecuredapplication/intrusiondetection/responseaction/CustomResponseAction.java
+++ src/main/java/org/meri/simpleshirosecuredapplication/intrusiondetection/responseaction/CustomResponseAction.java
@@ -9,24 +9,23 @@ import org.owasp.appsensor.intrusiondetection.ResponseAction;
 import org.owasp.appsensor.intrusiondetection.reference.DefaultResponseAction;
 
 public class CustomResponseAction implements ResponseAction {
-	
+
 	private final ResponseAction delegee = DefaultResponseAction.getInstance();
 
 	@Override
-  public boolean handleResponse(String action, AppSensorIntrusion currentIntrusion) {
+	public boolean handleResponse(String action, AppSensorIntrusion currentIntrusion) {
 		if ("warn".equals(action)) {
 			Exception securityException = currentIntrusion.getSecurityException();
 			String localizedMessage = securityException.getLocalizedMessage();
-			
+
 			ASUtilities asUtilities = APPSENSOR.asUtilities();
 			HttpServletRequest request = asUtilities.getCurrentRequest();
 			request.setAttribute("securityWarning", localizedMessage);
-			
+
 			return true;
 		}
-	  
-	  return delegee.handleResponse(action, currentIntrusion);
-  }
+
+		return delegee.handleResponse(action, currentIntrusion);
+	}
 
 }
-
